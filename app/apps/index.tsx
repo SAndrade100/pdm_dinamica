33af2b33d316bd6e79e24c63bc77b3b5a8c181a3
lucio <desouzaleitelucioweslley@gmail.com>
2025-03-07 11:54:46 -0300

    alterando style do botao e criando a tela home

--- app/apps/index.tsx
+++ app/apps/index.tsx
@@ -1,13 +1,41 @@
 import React from "react";
-import { Link } from "expo-router"
-import Container from "./style";
+import { Link, router } from "expo-router"
+import Container, { styles } from "./style";
+import { Image, StyleSheet, Text, View } from "react-native";
+import MainButton from "@/components/buttons/mainButton";
+
+
 export default function RootLayout() {
+
+  function loginRedirect() {
+    router.replace("/login")
+  }
+
+  function cardapioRedirect() {
+    router.replace("/cardapio")
+  }
 
   return (
     <>
-      <Container>
-        <Link href={"/"}>App</Link>
-      </Container>
+      <View style={styles.conteiner}>
+
+        <View style={styles.upStyle}>
+          <View>
+            <Text style={styles.textTituloStyle}>IFPB - DELIVERY</Text>
+            </View>
+            <Image style={styles.imageStyle} source={require("../../public/hg01.png")} resizeMode="contain"/>
+        </View>
+        <View style={styles.downStyle}>
+
+            <Text style={styles.textDescriptionStyle}>O Hamburguer mais delicioso da sua Cidade</Text>
+
+            <Text style={styles.textMainStyle}>Logou   ->   Pediu   ->   Chegou</Text>
+
+            <MainButton titulo="Fazer Login" type="filled" onPress={loginRedirect}/>
+            <MainButton titulo="Ver cardápio" type="empty" onPress={cardapioRedirect}/>
+        </View>
+      </View>
+
     </>
   );
 }
